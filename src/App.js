75ce5de8ae323c2c70bed76f0d76cc33a63d5805
Shantovai23 <hasibulhasan23sept@gmail.com>
2020-12-30 21:49:11 +0600

    backend-generated-with-commercejs

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 import Products from './components/products/Products'
 import Navbar from './components/Navbar/Navbar'
+import {commerce} from './lib/commerce'
 
 const App=()=>{
+    const [products, setProducts] = useState([]);
+
+    const fetchProducts = async () => {
+        const { data } = await commerce.products.list();
+    
+        setProducts(data);
+      };
+
+    useEffect(()=>{
+    fetchProducts()
+    },[])
+
+    console.log(products)
 
     return(<>
         <Navbar/>
-        <Products/>
+        <Products products={products}  />
     </>)
 }
 
--- src/components/products/Product/Product.js
+++ src/components/products/Product/Product.js
@@ -5,30 +5,30 @@ import useStyles from './style'
 
 const Product=({product})=>{
     const classes=useStyles()
-
-    return(<>
-        <Card className={classes.root}>
-            <CardMedia className={classes.media} image={product.image} title={Product.name}/>
-            <CardContent>
-                <div className={classes.cardContent}>
-                   <Typography variant='h5' gutterBottom>
-                       {product.name}
-                   </Typography>
-                   <Typography variant='h5'>
-                       {product.price}
-                   </Typography>
-                </div>
-                <Typography variant='body2' color='textSecondary'>
-                       {product.description}
-                   </Typography>
-            </CardContent>
-            <CardActions disableSpacing className={classes.cardActions}>
-                <IconButton aria-label='Add to Cart'>
-                    <AddShoppingCart/>
-                </IconButton>
-            </CardActions>
-        </Card>
-    </>)
+    
+  console.log(product)
+  
+  return(<>
+    <Card className={classes.root}>
+        <CardMedia className={classes.media} image={product.media.source} title={Product.name}/>
+        <CardContent>
+            <div className={classes.cardContent}>
+               <Typography variant='h6' gutterBottom>
+                   {product.name}
+               </Typography>
+               <Typography variant='h6'>
+                   {product.price.formatted_with_symbol}
+               </Typography>
+            </div>
+            <Typography dangerouslySetInnerHTML={{__html:product.description}}  variant='body2' color='textSecondary' />
+        </CardContent>
+        <CardActions disableSpacing className={classes.cardActions}>
+            <IconButton aria-label='Add to Cart'>
+                <AddShoppingCart/>
+            </IconButton>
+        </CardActions>
+    </Card>
+</>)
 }
 
 export default Product
